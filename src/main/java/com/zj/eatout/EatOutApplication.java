38f6a28b57e7c5ca907ab079fbfadcc5d5b930d5
YZJ <yzjjj0925@gmail.com>
2025-02-24 16:27:01 +0800

    test

---
+++ src/main/java/com/zj/eatout/EatOutApplication.java
@@ -0,0 +1,13 @@
+package com.zj.eatout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatOutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EatOutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zj/eatout/EatOutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zj.eatout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatOutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
